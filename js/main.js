a01fcc696ffe4cad5b7fc5eef15ad417d2c7a70a
ryuryu5121 <r.oshima.12.15@gmail.com>
2022-10-07 20:38:58 +0900

    change url

--- js/main.js
+++ js/main.js
@@ -45,20 +45,11 @@ $(function(){
     // カルーセル用 jQueryプラグイン「slick」の設定
     // マニュアル：https://kenwheeler.github.io/slick/
     $('.slick-area').slick({
-      arrows: false,
-      centerMode: true,
-      centerPadding: '100px',
-      slidesToShow: 3,
-      responsive: [
-        {
-          breakpoint: 768,
-          settings: {
-            centerPadding: '50px',
-            slidesToShow: 1
-          }
-        }
-      ]
-    });
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+      });
   
     /*=================================================
     スクロール時の画像フェード表示
